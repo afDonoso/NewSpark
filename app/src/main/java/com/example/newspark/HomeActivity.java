53180cb85214be8a5a3a0475af0f84780398b7ad
Andres Donoso <af.donoso@uniandes.edu.co>
2019-10-31 10:05:13 -0500

    Creado profile

--- app/src/main/java/com/example/newspark/HomeActivity.java
+++ app/src/main/java/com/example/newspark/HomeActivity.java
@@ -9,29 +9,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Calendar;
+
 public class HomeActivity extends AppCompatActivity {
 
-    public final static String GOOD_MORNING = "¡Buenos días,";
-    public final static String GOOD_AFTERNOON = "¡Buenas tardes,";
-    public final static String GOOD_NIGHT = "¡Buenas noches,";
+    public enum TimeOfDay{
+        Morning,
+        Noon,
+        Afternoon,
+        Night
+    }
+
+    public final static String GOOD_MORNING = "¡Buenos días, ";
+    public final static String GOOD_AFTERNOON = "¡Buenas tardes, ";
+    public final static String GOOD_NIGHT = "¡Buenas noches, ";
     public static final String TAG = "HomeActivity";
 
     private CardView cardView1;
     private CardView cardView2;
     private CardView cardViewWelcome;
     private TextView textViewGreetings;
+    private ImageView imageViewTimeDay;
 
     private String userEmail;
-    public String name;
 
     private FirebaseFirestore db;
 
@@ -44,6 +53,7 @@ public class HomeActivity extends AppCompatActivity {
 
         userEmail = getIntent().getStringExtra(SignupActivity.EMAIL_KEY);
         textViewGreetings = findViewById(R.id.textViewGreetings);
+        imageViewTimeDay = findViewById(R.id.imageViewTimeDay);
 
         final Context context = this;
 
@@ -86,9 +96,8 @@ public class HomeActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             for(QueryDocumentSnapshot document: task.getResult()) {
                                 Log.d(TAG, document.getId() + "=>" + document.getData());
-                                HomeActivity.this.setName(document.get(SignupActivity.NAME_KEY).toString());
-                                //HomeActivity.this.name = document.get(SignupActivity.NAME_KEY).toString();
-                                textViewGreetings.setText(GOOD_AFTERNOON + " " + name + "!");
+                                String name = document.get(SignupActivity.NAME_KEY).toString();
+                                updateUI(name);
                             }
                         } else {
                             Log.w(TAG, "Error getting documents", task.getException());
@@ -97,9 +106,47 @@ public class HomeActivity extends AppCompatActivity {
                 });
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * Actualiza la interfaz dada la hora del día.
+     */
+    public void updateUI(String userName) {
+        TimeOfDay timeOfDay = getTimeOfDay();
+        Log.d(TAG, timeOfDay.toString());
+
+        if(timeOfDay.equals(TimeOfDay.Morning)) {
+            textViewGreetings.setText(GOOD_MORNING + userName + "!");
+            imageViewTimeDay.setImageResource(R.drawable.day);
+        } else if(timeOfDay.equals(TimeOfDay.Noon)) {
+            textViewGreetings.setText(GOOD_AFTERNOON + userName + "!");
+            imageViewTimeDay.setImageResource(R.drawable.noon);
+        } else if(timeOfDay.equals(TimeOfDay.Afternoon)) {
+            textViewGreetings.setText(GOOD_AFTERNOON + userName + "!");
+            imageViewTimeDay.setImageResource(R.drawable.afternoon);
+        } else if(timeOfDay.equals(TimeOfDay.Night)) {
+            textViewGreetings.setText(GOOD_NIGHT + userName + "!");
+            imageViewTimeDay.setImageResource(R.drawable.night);
+        }
     }
 
+    /**
+     * Retorna el tiempo del día basándose en la hora del día.
+     * @return  Morning, si es entre las 4 y las 11.
+     *          Noon, si es entre las 12 y las 14.
+     *          Afternoon, si es entre las 15 y las 18.
+     *          Night, de lo contrario.
+     */
+    public TimeOfDay getTimeOfDay() {
+        int currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+        if (currentTime >= 4 && currentTime <= 11) {
+            return TimeOfDay.Morning;
+        } else if (currentTime >= 12 && currentTime <= 16) {
+            return TimeOfDay.Noon;
+        } else if (currentTime >= 17 && currentTime <= 18) {
+            return TimeOfDay.Afternoon;
+        } else {
+            return TimeOfDay.Night;
+        }
+    }
 
 }
